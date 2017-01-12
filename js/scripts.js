4f4af102b7e3de3b5d5aa5272902d0bd4d3069c2
Nicholas Mead and Shane Villars <shane@>
2017-01-12 09:22:50 -0800

    add jquery hide and show functionality to animal and home pages. Add a home page button

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
   $("#formSection form").submit(function(event) {
+    event.preventDefault()
     var animal = $("input:radio[name=animal]:checked").val();
+    $(".main").hide();
+    $(".options button").show();
 
     if (animal === "turtle") {
-      
+      $(".TurtlePage").show()
+    } else if (animal === "snake") {
+      $(".SnakePage").show()
+    } else {
+      $(".InsectPage").show()
     }
   })
-})
+
+  $(".options button").click(function() {
+    $(".TurtlePage").hide();
+    $(".SnakePage").hide();
+    $(".InsectPage").hide();
+    $(this).hide();
+    $(".main").show();
+  });
+});
