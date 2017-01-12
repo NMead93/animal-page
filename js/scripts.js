ef8e2c5865c25056a314b7c930b733a9b9f5e57d
Nicholas Mead and Shane Villars <nicholas@>
2017-01-12 08:57:58 -0800

    add initial page with 3 sections

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#formSection form").submit(function(event) {
+    var animal = $("input:radio[name=animal]:checked").val();
+
+    if (animal === "turtle") {
+      
+    }
+  })
+})
